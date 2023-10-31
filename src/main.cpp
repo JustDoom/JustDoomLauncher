26d8945efdefef7f7111f62e7826f9ac0602337f
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-10-31 21:42:16 +1000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,19 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (event.type == sf::Event::Resized)
+            {
+                // update the view to the new size of the window
+                sf::FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+                window.setView(sf::View(visibleArea));
+            }
         }
 
         window.clear();
